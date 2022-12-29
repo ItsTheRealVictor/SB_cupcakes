8a9b0207b8358b4fa0027caf6e959daf9d38712b
Victor Delgado <valexdelgado@gmail.com>
2022-12-29 12:01:34 -0800

    can now view all cakes using JS (styling is weird tho)

--- static/script.js
+++ static/script.js
@@ -1,10 +1,37 @@
 alert('fart')
 
+function makeHTML(cake){
+    // copied this right out of the home.html jinja loop
+    return `
+    <div class="col-4">
+                        
+        <div class="card my-2">
+            <div class="card-body">
+                <h5 class="card-title">${cake.flavor}</h5>
+                <h6 class="card-subtitle text-muted">Rating: ${cake.rating}</h6>
+                <h6 class="card-subtitle text-muted my-2">Size: ${cake.size}</h6>
+                <img class="card-img-top" src="${cake.image}" alt="">
+                <form style="display:inline" action="/delete/${cake.id}" method="POST">
+                    <button class="btn btn-sm btn-danger">DELETE CAKE</button>
+                </form>
+                
+            </div>
+        </div>
+    </div>
+    `
+}
+
+
 async function allCakes() {
     const res = await axios.get('http://127.0.0.1:5000/api/cupcakes')
     
-    for (let item of res.data.cakes)
-    console.log(item.flavor)
-}
+    
+    for (let cake of res.data.cakes){
 
+        let newCake = $(makeHTML(cake));
+        $('#cupcake-holder').append(newCake)
+        console.log(cake.flavor)
+
+    }
+}
 allCakes()
